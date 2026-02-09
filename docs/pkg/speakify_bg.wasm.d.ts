44fbbab1337d5e4a041652ab01a8e6fedac0dfc0
zeetee1235 <zeetee1235@gmail.com>
2026-02-09 09:20:53 +0900

    해치웠나?

---
+++ docs/pkg/speakify_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_speakifywasm_free: (a: number, b: number) => void;
+export const speakifywasm_convert: (a: number, b: number, c: number, d: number, e: number) => [number, number, number, number];
+export const speakifywasm_new: () => [number, number, number];
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
